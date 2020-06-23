7fdf0ddbaeab7d573f03024fe42f14b9595ae791
florin <costacheflorinalin@gmail.com>
2020-06-23 17:30:50 +0300

    conexiunea

---
+++ GeneralLibrary/IOManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralLibrary
+{
+    public class IOManager
+    {
+        public static void WriteFile(string message, string filename)
+        {
+            string fname = AppDomain.CurrentDomain.BaseDirectory + filename;
+            FileStream fs = File.Exists(fname) ?
+                                fs = File.Open(fname, FileMode.Append) :
+                                fs = File.Create(fname);
+
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLineAsync(message);
+
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
